1ef1d588d6e2558b46e2b13576a5d7db2354ec23
gnozebac <magbmail@gmail.com>
2021-08-03 18:57:03 -0500

    crear navbar como componente de pagina

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Navbar extends React.Component{
+    render(){
+        return (
+            <p>NavBar</p>
+        );
+    }
+  
+}
+export default Navbar;
+
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import Navbar from '../components/Navbar';
+
 class BadgeNew extends React.Component{
     render(){
-        return <h1>badgeNew</h1>;
+        return (
+            <div>
+                <Navbar />
+            </div>
+        );
     }
   
 }
