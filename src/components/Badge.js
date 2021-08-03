6ebabe9e882b82867e6a260311ab59deb8047e2e
Marco Guaman <magbmail@gmail.com>
2021-08-03 10:56:51 -0500

    APlicar estilos desde bootstrap

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -4,23 +4,23 @@ import confLogo from '../images/badge-header.svg';
 
 class Badge extends React.Component{
 render (){
-    return <div className="badge">
-        <div className="badge_header">
+    return <div className="Badge">
+        <div className="Badge__header">
         <img src ={confLogo} alt="logo suda"/>
         </div>
 
-        <div className="badge_section-name">
+        <div className="Badge__section-name">
             
-            <img className="badge_avatar" src ="https://www.gravatar.com/avatar/bc4094ed3653b18eba2198d3872abc84" alt="Avatar"/>
+            <img className="Badge__avatar" src ="https://www.gravatar.com/avatar/bc4094ed3653b18eba2198d3872abc84" alt="Avatar"/>
             <h1>Marco <br/> Guaman</h1>
         </div>
 
-        <div className="badge_section-info">            
+        <div className="Badge__section-info">            
             <p>Teacher</p>
             <p>@gnozebac</p>
         </div>
     
-        <div className="badge_footer">
+        <div className="Badge__footer">
             #magbdigital
         </div>
 
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Badge from './components/Badge';
-
+import 'bootstrap/dist/css/bootstrap.css';
 
 const container = document.getElementById('app');
 
