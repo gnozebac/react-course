af4d324ff908fcf1dea68d01e21e1d971d21ba50
gnozebac <magbmail@gmail.com>
2021-08-03 23:03:39 -0500

    crear form eventos input button submit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import logo from '../images/logo.png';
+import './styles/Navbar.css';
 class Navbar extends React.Component{
     render(){
         return (
-            <p>NavBar</p>
-        );
+            <div className="Navbar">
+                <div className="container-fluid">
+               <a className="Navbar__brand" href="/">
+                   <img className="Navbar__brand-logo" src={logo} alt= "logo"/>
+                   <span className="fw-light">Sudamericano </span>
+                   <span className="fw-bold">CasaAbiertaITS</span>
+                </a>    
+                </div>
+            </div>);
     }
   
 }
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,12 +1,37 @@
 import React from 'react';
 
 import Navbar from '../components/Navbar';
-
+import Badge from '../components/Badge';
+import BadgeForm from '../components/BadgeForm';
+import logo from '../images/logo-blanco-suda.png';
+import './styles/BadgeNew.css';
 class BadgeNew extends React.Component{
     render(){
         return (
             <div>
                 <Navbar />
+                <div className="BadgeNew__hero">
+                    <img className="img-fluid" src={logo} alt="logo"/>
+                </div>
+
+                <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <Badge
+                firstname="Marco"
+                lastname="Guamán"
+                tw="choloviejo"
+                job="Teacher"
+                
+              />
+            </div>
+            <div className="col-6">
+                <BadgeForm />
+            </div>
+
+          </div>
+        </div>
+
             </div>
         );
     }
