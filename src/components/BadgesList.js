886b97e23db607db3b205feed508be3c365f1ac3
gnozebac <magbmail@gmail.com>
2021-08-06 10:44:51 -0500

    llamado api data vacio y not found

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 import './styles/BadgesList.css';
 
+import { Link } from 'react-router-dom';
+
+
 class BadgesListItem extends React.Component {
   render() {
+
     return (
       <div className="BadgesListItem">
         <img
@@ -27,6 +31,16 @@ class BadgesListItem extends React.Component {
 
 class BadgesList extends React.Component {
   render() {
+    if (this.props.badges.length === 0) {
+      return (
+        <div>
+          <h3>No badges were found</h3>
+          <Link className="btn btn-primary" to="/badges/new">
+            Create new badge
+          </Link>
+        </div>
+      );
+    }
     return (
       <div className="BadgesList">
         <ul className="list-unstyled">
